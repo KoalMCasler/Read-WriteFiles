ff7e8e5655715e12f8209be1b54c2efe47b52ec6
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-11-15 15:24:18 -0400

    Started Read and Write

--- Read&WriteFiles/Program.cs
+++ Read&WriteFiles/Program.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Read_WriteFiles
 {
     internal class Program
     {
+        static string path = @"testText.txt"; //Relative file path
+        static string basicText;
+        static string[] mapRows;
         static void Main()
         {
-
-        }
-        static void ReadFile()
-        {
-
-        }
-        static void WriteFile()
-        {
-            
+            Console.WriteLine("Reading and writing files");
+            mapRows = File.ReadAllLines(path);
+            for(int y = 0; y < mapRows.Length; y++)
+            {
+                string mapRow = mapRows[y];
+                for(int x = 0; x < mapRow.Length; x++)
+                {
+                    char tile = mapRow[x];
+                    Console.Write(tile);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
         }
     }
 }
