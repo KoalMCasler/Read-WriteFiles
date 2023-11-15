ec1028586250e020fb603bb062086e30fe6a9bcf
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-11-15 13:39:44 -0400

    Initial commit

---
+++ Read&WriteFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_WriteFiles
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+        static void ReadFile()
+        {
+
+        }
+        static void WriteFile()
+        {
+            
+        }
+    }
+}
